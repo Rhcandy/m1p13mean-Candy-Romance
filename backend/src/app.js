cb977f7f02ed532ab4e6a1ac19f2b4c5de862448
Rhcandy <134164626+Rhcandy@users.noreply.github.com>
2026-01-26 23:29:53 +0300

    init project MEAN
    (backend express + frontend angular)

---
+++ backend/src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connexion à MongoDB
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connecté"))
+ .catch(err => console.log("Erreur MongoDB :", err));
+
+app.get("/", (req, res) => {
+  res.send("API MEAN OK");
+});
+
+app.listen(PORT, () => {
+  console.log("Serveur démarré sur le port " + PORT);
+});
