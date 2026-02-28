92286aecb3d05f08b0d3d7bde3eae169718891a7
Rhcandy <134164626+Rhcandy@users.noreply.github.com>
2026-02-28 08:35:54 +0300

    user brevo for email

--- backend/src/utils/send-mail.util.js
+++ backend/src/utils/send-mail.util.js
@@ -4,8 +4,6 @@ require("dotenv").config();
 const transport = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT),
-  secure: false,
-  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
